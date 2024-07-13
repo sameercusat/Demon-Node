a5cd4888f7f8d3b818497a62f3f287e6331ce3bb
sameercusat <113082184+sameercusat@users.noreply.github.com>
2024-07-13 15:01:02 +0530

    Initial Commit

---
+++ feature.js
@@ -0,0 +1,9 @@
+const myname= "sameer";
+const first ="ravindra";
+const second ="jadeja";
+const love = ()=>
+{
+    return `My Love is ${~~(Math.random()*100)}%`;
+}
+export {first,second,love};
+export default myname;
